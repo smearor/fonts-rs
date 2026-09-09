375ef6ca71ec8cce9d9ef4814a5ed4271621872d
Andreas Schaeffer <andreas@schaeffernet.de>
2026-09-09 21:39:28 +0200

    Panic-free

--- crates/nerd-fonts-generator/src/svg_path_builder.rs
+++ crates/nerd-fonts-generator/src/svg_path_builder.rs
@@ -26,19 +26,19 @@ impl Default for SvgPathBuilder {
 
 impl OutlineBuilder for SvgPathBuilder {
     fn move_to(&mut self, x: f32, y: f32) {
-        write!(self.path, "M {} {} ", x, y).unwrap();
+        let _ = write!(self.path, "M {} {} ", x, y);
     }
 
     fn line_to(&mut self, x: f32, y: f32) {
-        write!(self.path, "L {} {} ", x, y).unwrap();
+        let _ = write!(self.path, "L {} {} ", x, y);
     }
 
     fn quad_to(&mut self, x1: f32, y1: f32, x: f32, y: f32) {
-        write!(self.path, "Q {} {} {} {} ", x1, y1, x, y).unwrap();
+        let _ = write!(self.path, "Q {} {} {} {} ", x1, y1, x, y);
     }
 
     fn curve_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, x: f32, y: f32) {
-        write!(self.path, "C {} {} {} {} {} {} ", x1, y1, x2, y2, x, y).unwrap();
+        let _ = write!(self.path, "C {} {} {} {} {} {} ", x1, y1, x2, y2, x, y);
     }
 
     fn close(&mut self) {
