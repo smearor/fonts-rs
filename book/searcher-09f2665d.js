b69f3fefa0c1866e7041587c072f56534c8b9277
aschaeffer <aschaeffer@users.noreply.github.com>
2026-09-11 16:56:46 +0000

    deploy: dd5b8b32061eb550bba4f56a19c7b6c00507846a

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cd7d9998.js',
+                path_to_root + 'searchindex-0a1d92bd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
