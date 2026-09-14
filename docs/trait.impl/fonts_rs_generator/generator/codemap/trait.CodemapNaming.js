1d3bc657b50d9e9193e8aa2423ca24dfe227302f
aschaeffer <aschaeffer@users.noreply.github.com>
2026-09-14 02:47:06 +0000

    deploy: 525236159ae32b54c7c4b319ca0f9e3862ef4929

--- docs/trait.impl/fonts_rs_generator/generator/codemap/trait.CodemapNaming.js
+++ docs/trait.impl/fonts_rs_generator/generator/codemap/trait.CodemapNaming.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["fonts_rs_generator",[]],["nerd_fonts_generator",[["impl CodemapNaming for <a class=\"struct\" href=\"nerd_fonts_generator/generator/codemap/struct.IconsNaming.html\" title=\"struct nerd_fonts_generator::generator::codemap::IconsNaming\">IconsNaming</a>",0]]]]);
+    const implementors = Object.fromEntries([["fonts_rs_generator",[]],["nerd_fonts_generator",[["impl <a class=\"trait\" href=\"fonts_rs_generator/generator/codemap/trait.CodemapNaming.html\" title=\"trait fonts_rs_generator::generator::codemap::CodemapNaming\">CodemapNaming</a> for <a class=\"struct\" href=\"nerd_fonts_generator/generator/codemap/struct.IconsNaming.html\" title=\"struct nerd_fonts_generator::generator::codemap::IconsNaming\">IconsNaming</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[25,235]}
+//{"start":59,"fragment_lengths":[25,399]}
